a1dd12e26fbee45c8639447b56c0285335907da4
Jeffrey Millard <jeffrey.d.millard@gmail.com>
2017-05-20 13:40:21 -0600

    moved timer to class

--- src/example.cpp
+++ src/example.cpp
@@ -33,12 +33,19 @@ void ExampleClass::operations()
   // perform operation on uMat class member
   std::cout << "starting operations" << std::endl;
 
+  // timer
+  auto start = std::chrono::high_resolution_clock::now();
+
   // here is the syntax for cv::Mat and cv::cuda::GpuMat
   // cv::threshold(      src, dst, 128.0, 255.0, CV_THRESH_BINARY);
   // cv::cuda::threshold(src, dst, 128.0, 255.0, CV_THRESH_BINARY);
   // using the universal namespace alias, this command represents both:
   ucv::threshold(universalMat1, universalMat2, 128.0, 255.0, CV_THRESH_BINARY);
 
+  auto finish = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> elapsed = finish - start;
+  std::cout << "raw operation time: " << elapsed.count() << " s (excluding RAM/VRAM overhead)" << std::endl;
+  
   std::cout << "operations complete" << std::endl;
 }
 
@@ -47,7 +54,7 @@ cv::Mat ExampleClass::get_mat()
 {
   // The uMat type is defined in the header depending on the existence of CUDA.
   // Getting the cv::Mat from the uMat class member needs to consider this.
-  
+
 #if CUDA
   // uMat is cv::cuda::GpuMat
   // retrieve a deep copy of universalMat2 of type cv::Mat
--- src/main.cpp
+++ src/main.cpp
@@ -20,12 +20,7 @@ int main(int argc, char **argv)
   object.add_mat(frame);
 
   // perform operations on image
-  auto start = std::chrono::high_resolution_clock::now();
   object.operations();
-  auto finish = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> elapsed = finish - start;
-  std::cout << "raw operation time: " << elapsed.count() << " s (excluding RAM/VRAM overhead)" << std::endl;
-
 
   // retrieve image
   frame_thresh = object.get_mat();
